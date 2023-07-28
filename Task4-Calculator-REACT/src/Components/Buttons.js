8597752703b4b7c20c36a7ab17a5717db741be3c
Bhomesh <75837325+Bhomesh@users.noreply.github.com>
2023-07-28 19:19:32 +0530

    Delete Task4-Calculator-REACT directory

--- Task4-Calculator-REACT/src/Components/Buttons.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function Buttons({value, buttonClicked}){
-
-
-   return(
-        <button className="btn" onClick={buttonClicked}>{value}</button>
-
-   )
-}
--- Task4-Calculator-REACT/src/Components/Display.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-export default function Display({answer}){
-    return (
-        <div className="display">{answer}</div>
-    )
-}
